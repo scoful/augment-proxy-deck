c25fd6c0981b6e774ab2923c38ef1ea50c1c7c1a
scoful <1269717999@qq.com>
2025-08-15 15:46:46 +0800

    feat(analytics): 添加Vercel分析工具支持
    - 在package.json中添加@vercel/analytics依赖
    - 更新pnpm-lock.yaml以包含新依赖的版本信息
    - 在_app.tsx中引入Analytics组件并在应用中使用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { Geist } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "@/utils/api";
 
@@ -13,6 +14,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={geist.className}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 };
