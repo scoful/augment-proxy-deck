8f5efaa8c1537f3452ea67ebbc50a3a9db08d720
scoful <1269717999@qq.com>
2025-09-24 15:53:30 +0800

    Add Vercel Speed Insights for performance monitoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { Geist } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
@@ -14,6 +15,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <div className={geist.className}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 };
